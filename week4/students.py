3d36e86e8339f3f33e1fdcd296bb6d60c4d8b59c
Brian Barrins <G00299967@gmit.ie>
2021-02-17 21:19:03 +0000

    update for lack of print

---
+++ week4/students.py
@@ -0,0 +1,19 @@
+# A Program that reads in students
+# until the user enters a blank
+# and then prints them all out again
+
+students = []
+
+firstname = input("enter firstname (blank to quit): ").strip()
+while firstname != "":
+    student = {}
+    student["firstname"] = firstname
+    lastname = input("enter lastname: ").strip()
+    student["lastname"] = lastname
+    students.append(student)
+    # next student
+    firstname = input("enter firstname of next (blank to quit): ").strip()
+
+print ("here are the students you entered:")
+for currentStudent in students:
+    print ("{} {}".format(currentStudent["firstname"], currentStudent["lastname"]))
